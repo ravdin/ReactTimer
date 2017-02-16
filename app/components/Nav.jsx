6a8bf1bf7d72ab7d92af229f8928fe3492c25be8
Bill Ravdin <bill.ravdin@gmail.com>
2017-02-15 20:47:25 -0800

    Initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+  render: function () {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Timer App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeClassName="active-link">Timer</IndexLink>
+            </li>
+            <li>
+              <Link to="/" activeClassName="active" activeClassName="active-link">Countdown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">Created by Bill Ravdin</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = Nav;
